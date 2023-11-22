c124c1ae1674cdadaad49c2a21e0ecd85f20f1b2
Hamed Mohammadi <hamed@dehongi.com>
2023-11-22 22:35:38 +0330

    model

--- views.py
+++ views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.published.all()
+
+
+class PostDetailView(DetailView):
+    model = Post
